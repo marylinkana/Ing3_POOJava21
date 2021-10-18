3d3be6fc49ddb522310d2ba3abbf68aed236d6c4
amaaradji <a.maaradji@gmail.com>
2021-10-18 16:23:42 +0200

    tp3

---
+++ tp3/src/main/java/com/mycompany/to3/Carre.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.to3;
+
+/**
+ *
+ * @author amaaradji
+ */
+public class Carre extends Forme {
+    
+    double longueur; //longueur
+
+    public Carre() {
+        this.longueur = 0;
+    }
+
+    public Carre(double l) {
+        this.longueur = l;
+    }
+    
+    public Carre(double x, double y, double l) {
+        this.x = x;
+        this.y = y;
+        this.longueur = l;
+    }
+    
+    public Carre(Carre obj){
+        this.x = obj.x;
+        this.y = obj.y;
+        this.longueur = obj.longueur;
+    }
+    
+    
+    
+    
+    
+    
+    @Override
+    double getSurface() {
+        return longueur*longueur;
+    }
+    
+}
---
+++ tp3/src/main/java/com/mycompany/to3/Forme.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.to3;
+
+/**
+ *
+ * @author amaaradji
+ */
+public abstract class Forme {
+    double x, y;
+    abstract double getSurface();
+}
