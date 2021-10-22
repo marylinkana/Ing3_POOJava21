8de7a10b0c427be5d99eb1305d356b9cecb1a629
amaaradji <a.maaradji@gmail.com>
2021-10-22 16:50:48 +0200

    renamed pckage to3 to tp3

--- tp3/src/main/java/com/mycompany/tp3/myComparatorDistanceOrigine.java
+++ tp3/src/main/java/com/mycompany/tp3/myComparatorDistanceOrigine.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.to3;
+package com.mycompany.tp3;
 
 import java.util.Comparator;
 
